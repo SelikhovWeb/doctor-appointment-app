c224865bddfee5149da1fe4a88be32a0da19cd16
Михаил <misha@MacBookPro.local>
2024-08-27 01:17:01 +0200

    do not optimize images for production

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   images: {
     domains: ["localhost", "res.cloudinary.com", "lh3.googleusercontent.com"],
+    unoptimized: process.env.NODE_ENV === "production",
   },
 };
 
